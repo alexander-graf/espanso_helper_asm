5fc5714979970cbd8c98ec0da22e9e98d6c616b6
Alexander Graf <alexander-graf@users.noreply.github.com>
2025-06-18 02:55:45 +0200

    mit readme

--- build.bat
+++ build.bat
@@ -8,8 +8,21 @@ REM Assemble
 ml /c /coff %filename%.asm
 if errorlevel 1 goto :error
 
+REM Compile resources (if icon exists)
+if exist rabbit.ico (
+    rc espanso_helper.rc
+    if errorlevel 1 goto :error
+    set "resource_file=espanso_helper.res"
+) else (
+    set "resource_file="
+)
+
 REM Link using Microsoft linker
-\masm32\bin\link.exe /subsystem:console %filename%.obj \masm32\lib\kernel32.lib \masm32\lib\user32.lib \masm32\lib\masm32.lib /out:%filename%.exe
+if defined resource_file (
+    \masm32\bin\link.exe /subsystem:console %filename%.obj %resource_file% \masm32\lib\kernel32.lib \masm32\lib\user32.lib \masm32\lib\masm32.lib \masm32\lib\shell32.lib /out:%filename%.exe
+) else (
+    \masm32\bin\link.exe /subsystem:console %filename%.obj \masm32\lib\kernel32.lib \masm32\lib\user32.lib \masm32\lib\masm32.lib \masm32\lib\shell32.lib /out:%filename%.exe
+)
 if errorlevel 1 goto :error
 
 echo Build successful! Starting %filename%.exe...
